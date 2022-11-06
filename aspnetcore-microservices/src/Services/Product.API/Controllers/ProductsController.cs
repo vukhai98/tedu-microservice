6d2abb9311754e6a8cde0464fd426ef43bd88266
vukhai98 <vukhai27091998@gmail.com>
2022-11-06 18:52:51 +0700

    Feature: CRUD_ API cho Product.API

--- aspnetcore-microservices/src/Services/Product.API/Controllers/ProductsController.cs
+++ aspnetcore-microservices/src/Services/Product.API/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
+using AutoMapper;
 using Contracts.Common.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Product.API.Entities;
 using Product.API.Persistence;
 using Product.API.Repositories.Interfaces;
+using Shared.DTOs.Products;
+using System.ComponentModel.DataAnnotations;
 
 namespace Product.API.Controllers
 {
@@ -11,19 +14,82 @@ namespace Product.API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductRepository  _repository;
+        private readonly IProductRepository _repository;
 
-        public ProductsController(IProductRepository repository)
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
+        #region CRUD
         [HttpGet]
         public async Task<IActionResult> GetProducts()
         {
-            var result = await _repository.GetProducts();
+            var products = await _repository.GetProducts();
+
+            var result = _mapper.Map<IEnumerable<ProductDto>>(products);
 
             return Ok(result);
         }
+
+        [HttpGet("{id:long}")]
+        public async Task<IActionResult> GetProduct(long id)
+        {
+            var product = await _repository.GetByIdAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            var result = _mapper.Map<ProductDto>(product);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct([FromBody] CreateProductDto productDto)
+        {
+            var product = _mapper.Map<CatalogProduct>(productDto);
+            await _repository.CreateProduct(product);
+            await _repository.SaveChangesAsync();
+            var result = _mapper.Map<ProductDto>(product);
+            return Ok(result);
+        }
+
+        [HttpPut("{id:long}")]
+        public async Task<IActionResult> UpdateProduct([Required] long id, [FromBody] UpdateProductDto productDto)
+        {
+            var product = await _repository.GetProduct(id);
+
+            if (product == null)
+                return NotFound();
+
+            var updateProduct = _mapper.Map(productDto, product);
+
+            await _repository.UpdateProduct(updateProduct);
+            await _repository.SaveChangesAsync();
+
+            var result = _mapper.Map<ProductDto>(product);
+            return Ok(result);
+        }
+
+        #endregion
+
+        #region Additional Resources
+        [HttpGet("{productNo}")]
+        public async Task<IActionResult> UpdateProduct([Required] string productNo)
+        {
+            var product = await _repository.GetProductByNo(productNo);
+
+            if (product == null)
+                return NotFound();
+
+            var result = _mapper.Map<ProductDto>(product);
+
+            return Ok(result);
+        }
+        #endregion
     }
 }
