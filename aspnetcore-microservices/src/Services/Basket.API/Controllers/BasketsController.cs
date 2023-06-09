6daa80f1204669880b36f56072fc06b0684a1424
RUNSYSTEM\khaivm <khaivm@runsystem.net>
2023-06-09 14:20:34 +0700

    End Session 4

---
+++ aspnetcore-microservices/src/Services/Basket.API/Controllers/BasketsController.cs
@@ -0,0 +1,46 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.ComponentModel.DataAnnotations;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasketsController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        public BasketsController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet("{userName}")]
+        public async Task<IActionResult> GetBasketByUserName([Required]string userName)
+        {
+            var result = await _repository.GetBasketByUserName(userName);
+
+            return Ok(result ?? new Cart());
+        }
+
+        [HttpPost("updateBasket")]
+        public async Task<IActionResult> UpdateBasket([FromBody] Cart cart)
+        {
+            var options = new DistributedCacheEntryOptions()
+                                        .SetAbsoluteExpiration(DateTime.UtcNow.AddHours(1)) // Hạn của basket tồn tại trong bao lâu
+                                        .SetSlidingExpiration(TimeSpan.FromMinutes(5)); // Kiểm tra  xem bao nhiêu lâu chưa gọi đến key đấy
+            var result = await _repository.UpdateBasket(cart, options);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{userName}")]
+        public async Task<IActionResult> DeleteBasket([Required] string userName)
+        {
+            var result = await _repository.DeleteBasketFromUserName(userName);
+
+            return Ok(result);
+        }
+
+    };
+}
