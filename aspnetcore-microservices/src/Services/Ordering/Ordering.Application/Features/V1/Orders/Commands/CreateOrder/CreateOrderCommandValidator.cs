53ca03e0e012e0211991fa86e1e52da3e27d23b8
RUNSYSTEM\khaivm <khaivm@runsystem.net>
2023-06-26 17:51:12 +0700

    CRUD order Api

---
+++ aspnetcore-microservices/src/Services/Ordering/Ordering.Application/Features/V1/Orders/Commands/CreateOrder/CreateOrderCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.V1.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName).NotEmpty().WithMessage("{UserName} is required.")
+                          .NotNull().MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+
+            RuleFor(p => p.EmailAddress).EmailAddress().WithMessage("{EmailAddress} is invalid format.")
+                          .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.TotalPrice).NotEmpty().WithMessage("{TotalPrice} is required.")
+                        .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+        }
+    }
+}
---
+++ aspnetcore-microservices/src/Services/Ordering/Ordering.Application/Features/V1/Orders/Commands/CreateOrder/GetOrdersQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Common.Intrerfaces;
+using Ordering.Application.Common.Models;
+using Ordering.Application.Features.V1.Orders.Commands.CreateOrder;
+using Shared.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.V1.Orders.Queries.GetOrders
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, ApiResult<long>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        private readonly IOrderRepository _orderRepository;
+
+        public CreateOrderCommandHandler(IMapper mapper, IOrderRepository orderRepository, ILogger logger)
+        {
+            _mapper = mapper ?? throw new ArgumentException(nameof(mapper));
+            _orderRepository = orderRepository ?? throw new ArgumentException(nameof(orderRepository));
+            _logger = logger ?? throw new ArgumentException(nameof(logger));
+        }
+
+        public async Task<ApiResult<long>> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return new ApiResult<long>(false, 0);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return new ApiResult<long>(false, 0);
+            }
+        }
+    }
+}
